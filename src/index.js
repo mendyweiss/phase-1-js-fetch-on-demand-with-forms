94a33c90cbf6b7c85c591ad540dd00f7e67eb168
Mendy <mendyw94@gmail.com>
2022-04-23 02:29:31 -0400

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector('form');
 
+  inputForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      console.log(e);
+      eve = e.target['0'].value;
+
+      fetch(`http://localhost:3000/movies/${eve}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+  })
+}
+let eve;
 document.addEventListener('DOMContentLoaded', init);
